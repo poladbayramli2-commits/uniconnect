3015eb6413e2059e7fedd0078d0fad555fe695af
bpolad06 <bpolad.06@gmail.com>
2026-04-20 05:03:39 +0400

    UniConnect ilk versiya

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.js";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { FirebaseBanner } from "./components/FirebaseBanner.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <FirebaseBanner />
         <App />
       </AuthProvider>
     </BrowserRouter>
